e18d9c2553e84ac7c44f441014dd2bb3d6271657
chichoon <chichoon.choi@gmail.com>
2022-10-02 19:36:22 +0900

    Chore: 린터, 웹팩, 바벨 설정

---
+++ webpack.config.cjs
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const currentMode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+  entry: { index: './src/index.ts' }, // 진입점 (1개)
+  output: {
+    path: path.resolve(__dirname, '/dist'), // 번들링 파일 저장 폴더
+    filename: 'bundle.js', // 번들링 파일 이름
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'], // SASS 로더
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' }), new MiniCssExtractPlugin()],
+  mode: currentMode,
+  devServer: {
+    static: { directory: path.resolve(__dirname, '/dist') },
+    compress: true,
+    port: 9000,
+    open: true,
+    compress: true,
+    host: 'localhost',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, '/src/components'),
+      '@pages': path.resolve(__dirname, '/src/pages'),
+    },
+  },
+};
