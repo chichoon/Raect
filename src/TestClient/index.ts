5e43a11ded56101886460f1776d7ca7b453ec167
chichoon <chichoon.choi@gmail.com>
2022-10-03 23:37:59 +0900

    Feat: index.ts에서 렌더링 시도

--- src/TestClient/index.ts
+++ src/TestClient/index.ts
@@ -1,12 +1,10 @@
-import { DomNodeClass } from '@raect/VirtualDom/DomNodeClass';
+import { createRoot } from '@raect/VirtualDom/createRoot';
+import { VirtualDOMNode } from '@raect/VirtualDom/VirtualDOMNode';
 
-function App() {
-  const virtualDOM = new DomNodeClass('div', { class: 'title-wrapper' }, [
-    new DomNodeClass('h1', {}, ['환영합니다!']),
-    new DomNodeClass('p', {}, ['이곳은 chichoon 블로그 입니다']),
-  ]);
-
-  document.querySelector('#root')?.appendChild(virtualDOM.createDOMElement());
-}
-
-App();
+const root = createRoot(document.getElementById('root'));
+root.render(
+  new VirtualDOMNode('div', { class: 'title-wrapper' }, [
+    new VirtualDOMNode('h1', {}, ['환영합니다!']),
+    new VirtualDOMNode('p', {}, ['이곳은 chichoon 블로그 입니다']),
+  ])
+);
