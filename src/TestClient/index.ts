3e9d0d0a816bb47fd18bfa5e44ec858099fc47b3
chichoon <chichoon.choi@gmail.com>
2022-10-04 19:53:23 +0900

    Feat: 가상 DOM 하나 더 추가

--- src/TestClient/index.ts
+++ src/TestClient/index.ts
@@ -1,10 +1,25 @@
-import { createRoot } from '@raect/VirtualDom/createRoot';
-import { VirtualDOMNode } from '@raect/VirtualDom/VirtualDOMNode';
+import { createRoot, VirtualDOMNode } from '@raect/VirtualDom';
+
+const oldVirtualDOM = new VirtualDOMNode('div', { class: 'title-wrapper' }, [
+  new VirtualDOMNode('h1', {}, ['환영합니다!']),
+  new VirtualDOMNode('p', {}, ['이곳은 chichoon 블로그 입니다']),
+]);
+
+const newVirtualDOM = new VirtualDOMNode('div', { class: 'title-wrapper' }, [
+  new VirtualDOMNode('h1', {}, ['안녕히 가세요!']),
+  new VirtualDOMNode('p', {}, ['다음에도 또 방문해 주세요']),
+]);
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-  new VirtualDOMNode('div', { class: 'title-wrapper' }, [
-    new VirtualDOMNode('h1', {}, ['환영합니다!']),
-    new VirtualDOMNode('p', {}, ['이곳은 chichoon 블로그 입니다']),
-  ])
-);
+root.render(oldVirtualDOM);
+
+// setInterval(() => {
+//   const temp = oldVirtualDOM;
+//   oldVirtualDOM = newVirtualDOM;
+//   newVirtualDOM = temp;
+//   root.render(oldVirtualDOM);
+// }, 1000);
+
+const span1 = document.createElement('span');
+const span2 = document.createElement('span');
+document.getElementsByTagName('h1')[0].replaceChildren(span1, span2);
