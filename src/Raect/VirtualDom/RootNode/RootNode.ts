943a9e665f6186a92c99e9bf4625449638b066ce
chichoon <chichoon.choi@gmail.com>
2022-10-04 19:50:26 +0900

    Rename: RootNode 폴더 이동
    - update 함수 wip

---
+++ src/Raect/VirtualDom/RootNode/RootNode.ts
@@ -0,0 +1,28 @@
+import { VirtualDOMNode } from '@raect/VirtualDom/VirtualDOMNode';
+import { updateEachNode } from './updateEachNode';
+
+type VirtualDOMNodeType = VirtualDOMNode | null;
+
+export class RootNode {
+  #rootDOMNode: Element;
+  #virtualDOMNode: VirtualDOMNodeType;
+  constructor(rootNode: Element) {
+    this.#rootDOMNode = rootNode;
+    this.#virtualDOMNode = null;
+  }
+
+  render(childNode: VirtualDOMNode) {
+    this.#rootDOMNode.replaceChildren(childNode.createDOMElement());
+    this.#virtualDOMNode = childNode;
+  }
+
+  unmount() {
+    this.#rootDOMNode.innerHTML = '';
+    this.#virtualDOMNode = null;
+  }
+
+  update(newChildNode: VirtualDOMNodeType) {
+    // 현재 Virtual DOM과 실제 DOM을 업데이트 하는 함수
+    updateEachNode(this.#rootDOMNode, this.#virtualDOMNode, newChildNode, 0);
+  }
+}
