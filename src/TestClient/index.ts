994e5af0c573d04733fe094b573d8d23629ff338
chichoon <chichoon.choi@gmail.com>
2022-10-04 21:28:04 +0900

    Feat: 테스트용 index.ts 수정

--- src/TestClient/index.ts
+++ src/TestClient/index.ts
@@ -1,25 +1,21 @@
 import { createRoot, VirtualDOMNode } from '@raect/VirtualDom';
 
-const oldVirtualDOM = new VirtualDOMNode('div', { class: 'title-wrapper' }, [
-  new VirtualDOMNode('h1', {}, ['환영합니다!']),
-  new VirtualDOMNode('p', {}, ['이곳은 chichoon 블로그 입니다']),
+let oldVirtualDOM = new VirtualDOMNode('div', { class: 'title-wrapper' }, [
+  new VirtualDOMNode('h1', { class: 'title-header', id: 'header' }, ['환영합니다!']),
+  new VirtualDOMNode('p', { class: 'title-header', id: 'header-p' }, ['이곳은 chichoon 블로그 입니다']),
 ]);
 
-const newVirtualDOM = new VirtualDOMNode('div', { class: 'title-wrapper' }, [
-  new VirtualDOMNode('h1', {}, ['안녕히 가세요!']),
-  new VirtualDOMNode('p', {}, ['다음에도 또 방문해 주세요']),
+let newVirtualDOM = new VirtualDOMNode('div', { class: 'title-wrapper' }, [
+  new VirtualDOMNode('h1', { class: 'title-header', id: 'header' }, [new VirtualDOMNode('i', {}, ['안녕히 가세요!'])]),
+  new VirtualDOMNode('p', { class: 'title-sub-header', id: 'header-p' }, ['다음에도 또 방문해 주세요']),
 ]);
 
 const root = createRoot(document.getElementById('root'));
 root.render(oldVirtualDOM);
 
-// setInterval(() => {
-//   const temp = oldVirtualDOM;
-//   oldVirtualDOM = newVirtualDOM;
-//   newVirtualDOM = temp;
-//   root.render(oldVirtualDOM);
-// }, 1000);
-
-const span1 = document.createElement('span');
-const span2 = document.createElement('span');
-document.getElementsByTagName('h1')[0].replaceChildren(span1, span2);
+setTimeout(() => {
+  const temp = oldVirtualDOM;
+  oldVirtualDOM = newVirtualDOM;
+  newVirtualDOM = temp;
+  root.update(oldVirtualDOM);
+}, 1000);
